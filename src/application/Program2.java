e69e26e5628f4bb6333f9b8e0256485cab73b79c
Giordana Rubin <giordana1g@gmail.com>
2024-02-13 12:46:28 -0300

    department insert and findById implemented

---
+++ src/application/Program2.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("=== TEST 1: department findById ===");
+		Department department = departmentDao.findById(1);		
+		System.out.println(department);
+
+		System.out.println("\n=== TEST 2: department insert ===");
+		Department newDepartment = new Department(null, "Food");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id = " + newDepartment.getId());
+	}
+
+}
